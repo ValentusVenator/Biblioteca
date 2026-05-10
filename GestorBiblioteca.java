ebe265c26230020cd04dab235434f437780ac69a
ValentusVenator <valentin.olivieri@insbaixcamp.cat>
2026-05-10 16:31:29 +0200

    Add files via upload

--- GestorBiblioteca.java
+++ GestorBiblioteca.java
@@ -5,9 +5,16 @@ import java.util.List;
 public class GestorBiblioteca {
 
 	private List<Prestamo> prestamos;
+	private List<Usuario> usuarios;
 
 	public GestorBiblioteca(){
 		this.prestamos = new ArrayList<>();
+	}
+	
+	public void listarUsuarios() {
+	    for (int i = 0; i < usuarios.size(); i++) {
+	        System.out.println(usuarios.get(i));
+	    }
 	}
 
 	public void prestarLlibre(Usuario usuario, Libro libro) {
@@ -17,15 +24,17 @@ public class GestorBiblioteca {
 		Prestamo prestamo = new Prestamo(usuario, libro, LocalDate.now());
 		prestamos.add(prestamo);
 
-		usuario.afegirLlibre(libro);
+		usuario.anadirLibro(libro);
 
 		System.out.println(usuario.getNombre() + " ha agafat el llibre: " + libro.getTitulo());
 		
 		// controlador de limite de libros para el usuario
-		if (!usuario.potAgafarLlibre()) {
+		if (!usuario.puedeAnadirLibro()) {
 			System.out.println("L'usuari ha arribat al límit de llibres.");
 			return;
 		}
 		
 	}
+
+
 }
