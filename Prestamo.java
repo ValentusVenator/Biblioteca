843eebca8e161592f1cd817ac9e8e866a6dbc489
joelpuyo-source <joel.puyo@insbaixcamp.cat>
2026-05-10 17:03:38 +0200

    version 1.0

--- Prestamo.java
+++ Prestamo.java
@@ -1,6 +1,8 @@
-package biblioteca;
-
 import java.time.LocalDate;
+
+    /**
+     *     Creacion de las variables de Prestamo
+     */
 
 public class Prestamo  {
     private Usuario usuario;
@@ -8,6 +10,10 @@ public class Prestamo  {
     private LocalDate fechaPrestamo;
     private LocalDate fechaDevolucion;
     private Libro[] listaLibro;
+
+    /**
+     *   Declaramos el constructor de la clase
+     */
 
     public Prestamo(Usuario usuario, Libro libro, LocalDate fechaPrestamo) {
         this.usuario = usuario;
@@ -19,15 +25,31 @@ public class Prestamo  {
         System.out.println("Tienes que devolver el libro antes de: " + fechaDevolucion);
     }
 
+    /**
+     * Metodo que recoge el libro que se quiere tomar prestado
+     * añadiendo la fecha de prestamo y la fecha en la que se debe devolver
+     * @param libro sabemos que Libro es
+     */
+
 	public void gestionarPrestamo(Libro libro) {
 		usuario.anadirLibro(libro);
         System.out.println("Has alquilado el libro: " + libro.getTitulo() + " el dia: " + fechaPrestamo);
         System.out.println("Tienes que devolver el libro antes de: " + fechaDevolucion);
     }
 
+    /**
+     *  Metodo que define el numero maximo de libros que se puede tener a la vez
+     * @param numeroMaximo Define el numero maximo de libros
+     */
+
     public void maximoLibros(int numeroMaximo) {
     	this.listaLibro[10] = listaLibro[numeroMaximo];        	
     }
+
+
+    /**
+     *     Getters y Setters
+     */
 
     public Usuario getUsuario() {
         return usuario;
@@ -45,6 +67,4 @@ public class Prestamo  {
 	public void setListaLibro(Libro[] listaLibro) {
 		this.listaLibro = listaLibro;
 	}
-
-	
 }
