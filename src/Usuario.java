1d9f76ee2fed4cb21e375e5d2912b206bf87e9c8
ValentusVenator <valentin.olivieri@insbaixcamp.cat>
2026-05-10 16:21:36 +0200

    Add files via upload

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,18 +1,22 @@
 import java.util.ArrayList;
+
 import java.util.List;
+import java.util.Scanner;
+
 
 public class Usuario {
 	private String nombre;
 	private List<Libro> librosPrestados;
+
 	public Usuario(String nombre) {
 		this.nombre = nombre;
 		this.librosPrestados = new ArrayList<>();
 	}
-	
+
 	public String getNombre() { 
 		return nombre; 
 	}
-	
+
 	public List<Libro> getLibrosPrestados() { 
 		return librosPrestados;
 	}
@@ -27,8 +31,28 @@ public class Usuario {
 		this.nombre = nombre;
 	}
 
+	public void borrarUsuario(Usuario usuario) {
+		usuario = null;
+	}
+
+	public void buscarUsuarios(Usuario u) {
+		System.out.println(u);
+	}
+
 	public void setLibrosPrestados(List<Libro> librosPrestados) {
 		this.librosPrestados = librosPrestados;
 	}
 	
+	public String toString() {
+		return "El usuario se llama: " + this.nombre;
+	}
+	
+	public boolean puedeAnadirLibro() {
+		if(this.librosPrestados.size()>=10) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+
 }
